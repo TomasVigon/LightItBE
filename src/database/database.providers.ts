a170bed272f5824f0c6fccd8253fbd15718f45c2
vaigon <vigon.t@nuqlea.com>
2024-06-18 21:00:30 -0300

    fix: add docker without env vars

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,7 +6,7 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
+        host: 'mysql_db',
         port: 3306,
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
